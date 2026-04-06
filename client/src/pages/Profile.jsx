0b3ac64e1db76602f6146990286a8ab6bf046673
Aser Alemu <asertesfaye@hotmail.com>
2026-04-06 19:36:04 +0300

    avatar upload don partially needs to be set on mongodb instead of firebase

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { use } from "react";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import {app} from "../firebase";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const fileInputRef = useRef(null);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUplaodError, setFileUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload({ target: { files: [file] } });
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + "_" + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed", (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFilePerc(Math.round(progress));
+      console.log("Upload is " + progress + "% done");
+    });
+    (error) => {
+      setFileUploadError(true);
+    };
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setFormData({ ...formData, avatar: downloadURL})
+      })
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form action="" className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          name=""
+          id=""
+          ref={fileInputRef}
+          hidden
+          accept="image/*"
+        />
         <img
+          onClick={() => fileInputRef.current.click()}
           src={currentUser.avatar}
           alt="Avatar"
           className="cursor-pointer rounded-full h-24 w-24 object-cover self-center mt-2"
