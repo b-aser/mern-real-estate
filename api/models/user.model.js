5b23389b11f6ef06d1ce926af7048d47c6846fd6
Aser Alemu <asertesfaye@hotmail.com>
2026-04-06 19:01:22 +0300

    complete profile page UI

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
     },
     avatar: {
       type: String,
-      default: "",
+      default: "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3467.jpg",
     },
   },
   {
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form action="" className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="Avatar"
+          className="cursor-pointer rounded-full h-24 w-24 object-cover self-center mt-2"
+        />
+        <input
+          type="text"
+          id="username"
+          placeholder={currentUser.username}
+          defaultValue={currentUser.username}
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          id="email"
+          placeholder={currentUser.email}
+          defaultValue={currentUser.email}
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer hover:underline">
+          Delete Account
+        </span>
+        <span className="text-red-700 cursor-pointer hover:underline">
+          Sign Out
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
