3ffd114a1d480ded1418b90a427512386c5b69a4
Aser Alemu <asertesfaye@hotmail.com>
2026-04-06 16:02:26 +0300

    Partially complete the Profile page and athentication for pages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
         <Link rel="stylesheet" to="/" >
-          <span className="text-slate-500">Bash</span>
+          <span className="text-slate-500">Bash-</span>
           <span className="text-slate-700">Estate</span>
         </Link>
         </h1>
@@ -29,8 +31,14 @@ const Header = () => {
           <Link to="/about">
             <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
           </Link>
-          <Link to="/signin">
-            <li className="  text-slate-700 hover:underline">Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img src={currentUser.avatar} alt="Profile" className="w-7 h-7 rounded-full object-cover" />
+            ) : (
+              <li className="  text-slate-700 hover:underline"> Sign In</li>
+            )}
+
+            
           </Link>
         </ul>
       </div>
